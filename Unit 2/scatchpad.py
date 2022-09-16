dae78de282c8b1a96900ad8ce0ee1b0cadfdbbbc
Shaochen Huang <ifwonderland@gmail.com>
2022-09-16 14:49:03 -0700

    Update names for better content reflection

--- Unit 2/scatchpad.py
+++
@@ -1,8 +0,0 @@
-#PCA vs MDS
-import numpy as np
-x = np.matrix([[1,1],[1,-1],[-1,1]])
-B = np.matmul(x, x.transpose())
-#PCA of B
-w, v = np.linalg.eig(B)
-v1 = v[:,0]/np.linalg.norm(v[:,0])
-print(v1)
